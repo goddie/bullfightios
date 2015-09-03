929b5068fb54400e4cab87b5d0108b97645c17e6
goddie <goddie2046@gmail.com>
2015-09-03 12:38:50 +0800

    no message

---
+++ bullfight/TabBar.h
@@ -0,0 +1,13 @@
+//
+//  TabBar.h
+//  bullfight
+//
+//  Created by goddie on 15/9/3.
+//  Copyright (c) 2015年 santao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBar : UIView
+
+@end
---
+++ bullfight/TabBar.m
@@ -0,0 +1,21 @@
+//
+//  TabBar.m
+//  bullfight
+//
+//  Created by goddie on 15/9/3.
+//  Copyright (c) 2015年 santao. All rights reserved.
+//
+
+#import "TabBar.h"
+
+@implementation TabBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
